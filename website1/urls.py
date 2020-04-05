5366534dbab2af977a6a4aec65ef6329fc379f64
BMNCodes <63168524+BMNCodes@users.noreply.github.com>
2020-04-05 13:43:12 +0430

    Update urls.py

--- website1/urls.py
+++ website1/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
 from . import views
 
-#from django.contrib import admin
-#from accounts.views import home_view, signup_view
-
-
 
 urlpatterns=[
     path('', views.news_list, name= 'news_list'),
     path('<int:year>/<int:month>/<int:day>/<str:news>/',views.news_detail, name= 'news_detail'),
 
-#    path('admin/', admin.site.urls),
-#    path('', home_view, name="home"),
-#    path('signup/', signup_view, name="signup")
 ]
